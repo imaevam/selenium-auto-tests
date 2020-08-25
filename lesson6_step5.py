6376f658fc4d1020c0eaf746d3818ecfe6878f7c
imaevam <66080125+imaevam@users.noreply.github.com>
2020-08-25 17:17:12 +0300

    Update lesson6_step5.py

--- lesson6_step5.py
+++ lesson6_step5.py
@@ -1,28 +1,25 @@
 from selenium import webdriver
+import math
 import time
 
-
 link = "http://suninjuly.github.io/find_link_text"
-browser = webdriver.Chrome()
-browser.get(link)
-link = browser.find_element_by_partial_link_text('224592')
-link.click()
 
 try:
+    browser = webdriver.Chrome()
+    browser.get(link)
+    link1 = browser.find_element_by_link_text(str(math.ceil(math.pow(math.pi, math.e)*10000)))
+    link1.click()
     input1 = browser.find_element_by_tag_name("input")
     input1.send_keys("I")
     input2 = browser.find_element_by_name("last_name")
     input2.send_keys("P")
-    input3 = browser.find_element_by_css_selector(".form-control.city")
+    input3 = browser.find_element_by_class_name("form-control.city")
     input3.send_keys("S")
     input4 = browser.find_element_by_id("country")
     input4.send_keys("R")
     button = browser.find_element_by_css_selector("button.btn")
     button.click()
-
 finally:
-    time.sleep(30)
+    time.sleep(5)
     # закрываем браузер после всех манипуляций
     browser.quit()
-
-# не забываем оставить пустую строку в конце файла
